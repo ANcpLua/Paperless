d9326256e06d73a6e5dda2f3045cbaf09e2e8dd4
ANcpLua <anfh22@outlook.com>
2024-10-27 12:51:02 +0100

    removed comments so sprint 1 is still available eventho we dont need the hardcodeded stuff anymore just for points

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17805b5133a8cd5a08c102bac17420adc82593df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef7e8267429c70c83101d698554ddf1646492636")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
