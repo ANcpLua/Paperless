8916944ceace7b4b4742af0b522cf79690e4bcad
ancplua <anfh22@outlook.com>
2025-07-29 15:36:19 +0200

    ewew

--- PaperlessServices/obj/Debug/net10.0/PaperlessServices.AssemblyInfo.cs
+++ PaperlessServices/obj/Debug/net10.0/PaperlessServices.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaperlessServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd64c61b890d074d43d05260f9e07d9d9f3d2808")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d02b78d62638c23c6e09aa011a089b40bb5c2cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaperlessServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaperlessServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
